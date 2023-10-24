11df4fbced11909f4da566d98b17bcf0ffb53101
Vehementz <vale-tudo91@sfr.fr>
2023-10-24 20:53:02 +0200

    can finded a specific post with param in the url

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const postController = require('../controllers/post');
-
 const router = express.Router();
 
-router.get('/', postController.getPosts);
+router.get('/api/posts', postController.getPosts);
+router.get('/post/:title', postController.getPostByTitle);
 
 module.exports = {
     postRoutes: router
