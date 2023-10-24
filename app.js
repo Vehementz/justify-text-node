3e18179a9a2125e2fba0d69126b8755950c5cf3b
Vehementz <vale-tudo91@sfr.fr>
2023-10-24 20:03:59 +0200

    app initialized

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello world from js")
+})
+
+const port  = 8080
+app.listen(port, () => {console.log(` A node js api listen to that port`)});
