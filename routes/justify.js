89fbec3c4b44decf6bc93d399bed1f2bf2b94a1f
Vehementz <vale-tudo91@sfr.fr>
2023-10-25 12:40:58 +0200

    text justified, some work to prevent xss attack

--- routes/justify.js
+++ routes/justify.js
@@ -1,41 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const { justifyText } = require('../utils/justifText');
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
 
-router.use(express.text({ type: 'text/plain' })); // Middleware pour parser le contenu de type text/plain
+const window = (new JSDOM('')).window;
+const DOMPurify = createDOMPurify(window);
+
+router.use(express.text({ type: 'text/plain' })); 
 
 router.get('/api/justify', (req, res) => {
-    const text = req.query.text;
+    const text = DOMPurify.sanitize(req.query.text);
 
     if (!text) {
         return res.status(400).json({ error: 'Texte requis' });
     }
 
     const justifiedText = justifyText(text);
-    const responseText = `<p style="text-align: justify; width: 600px; ">${justifiedText}</p>`;
+    const responseText = `<p style="text-align: justify; width: 600px;">${justifiedText}</p>`;
 
-    router.use(express.text({ type: 'text/html' }));
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
 
     res.send(responseText);
 });
 
-
 router.post('/api/justify', (req, res) => {
-    const text = req.body;
+    const text = DOMPurify.sanitize(req.body);
 
     if (!text) {
         return res.status(400).json({ error: 'Texte requis' });
     }
 
-
-    const justifiedText = justifyText(req.body);
+    const justifiedText = justifyText(text);
     console.log('Justified text:', justifiedText);
 
     res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+
     res.send(`<p style="text-align: justify; width: 600px;">${justifiedText}</p>`);
 });
-
-
-
 
 module.exports = router;
